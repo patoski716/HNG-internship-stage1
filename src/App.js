158483cfbb60d20cda7de321fc202f1f10dcb62c
chiscocode <patrickchukwudifu716@gmail.com>
2022-11-03 20:22:24 +0100

    updating and adding a contact page to the app

--- src/App.js
+++ src/App.js
@@ -1,48 +1,23 @@
-import Head from './components/Head'
-import Footer from './components/Footer'
-import GitHub from './images/GitHub.png'
-import Slack from './images/slack.png'
+import React from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './components/Home'
+import Contact from './components/Contact'
 
-const links = [
-  {id : "twitter", title : "Twiter Link", href : "https://twitter.com/patrick97856974"},
-  {id : "btn_zuri", title : "Zuri Team", hidden: true, href : "https://training.zuri.team/"},
-  {id : "books", title : "Zuri Book", hidden: true, href : "http://books.zuri.team"},
-  {id : "book_python", title : "Python Books", href : "http://books.zuri.team/python-for-beginners?ref_id=patoski"},
-  {id : "pitch", title : "Background Check for Coders", href : "https://background.zuri.team"},
-  {id : "book_design", title : "Design Books", href : "https://books.zuri.team/design-rules"}
-]
 
-function App() {
+
+const App = () => {
   return (
-    <div className="container">
-      
-      <Head/>
-     
-      {links.map((post)=>
-        <div key={post.id} className="card rounded  text-center mt-3">
-        <a href={post.href} type='button' id={post.id} style={{height:'50px',width:'100%',backgroundColor:'#EAECF0',textDecoration:'none',color:'black',border:'#EAECF0',justifyContent:'center'}}>
-            <p className='mt-3'>{post.title}</p>
-        </a>
-      </div>
-      )}
-      
-      <div className="row mb-5">
-        <div className="col-md-4"></div>
-          <div className="col-md-4 mx-5">
-            <div className="mt-4 mx-5 px-5">
-              <div className="port">
-              <span className='point text-center mx-auto p-3'><img src={Slack} alt="github"  style={{height:'20px'}}/></span>
-            <span className='point text-center mx-auto'><img src={GitHub} alt="github"  style={{height:'25px'}}/></span>
-
-              </div>
-              </div>
-            </div>
-          </div>
-      <Footer/>
-        
-
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/contact" element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Contact.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Footer from './Footer'
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <section id="register" className=" mt-5 py-5 mb-5 pb-5">
+        
+          <div className="row">
+            <div className="col-md-6 mx-auto">
+            <p style={{fontWeight:'bold',fontSize:'26px'}}>Contact Me</p>
+            <p>Hi there, contact me to ask me about anything you have in your mind. </p>
+            <form>
+              <div className="row">
+                <div className="col-md-6">
+                <label htmlFor="firstname" style={{fontWeight:'bold'}}>First Name</label>
+                <input type="text" id='first_name' className="form-control" placeholder="Enter your first name" required="required" />
+                </div>
+                <div className="col-md-6">
+                <label htmlFor="lastname" style={{fontWeight:'bold'}}>Last Name</label>
+                <input type="text" id='last_name' className="form-control" placeholder="Enter your last name" required="required" />
+                </div>
+                <div className="col-md-12">
+                <label htmlFor="email" style={{fontWeight:'bold'}}>Email</label>
+                <input type="email" id='email' className="form-control" placeholder="yourname@email.com" required="required"/>
+
+                <label htmlFor="message" id='message' style={{fontWeight:'bold'}}>Message</label>
+                <textarea className="form-control" rows="5" placeholder="send me a messageand i'll reply you as soon as possible" required="required"/>
+                
+                <input type="checkbox" className='mt-3 mx-2'/> You agree to providing your data to <span>{"{"}</span>name<span>{"}"}</span> who may contact you  
+                </div>
+                <div className="col-md-12">
+                <button id='btn__submit' className="btn btn-primary mt-4" style={{height:'40px',width:'100%',textDecoration:'none',color:'white',border:'#EAECF0',justifyContent:'center'}}>Send message</button>
+                </div>
+              </div>
+
+            </form>
+
+            </div>
+          </div>
+      </section>
+      <Footer/>
+
+    </div>
+
+  )
+}
+
+export default Contact
