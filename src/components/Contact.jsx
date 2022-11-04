029ccff3280df08d12ee29e1169759f864a574ee
chiscocode <patrickchukwudifu716@gmail.com>
2022-11-04 22:52:32 +0100

    update my contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -59,33 +59,33 @@ const Contact = () => {
         
           <div className="row">
             <div className="col-md-6 mx-auto">
-            <p style={{fontWeight:'bold',fontSize:'26px'}}>Contact Me</p>
+            <p className="text-black" style={{fontWeight:'bold',fontSize:'2rem'}}>Contact Me</p>
             <p>Hi there, contact me to ask me about anything you have in your mind. </p>
             <form>
               <div className="row">
                 <div className="col-md-6">
                 <label htmlFor="firstname" style={{fontWeight:'bold'}}>First Name</label>
-                <input type="text" id='first_name' className="form-control" placeholder="Enter your first name" required="required" value={post?.first_name} onChange={(e) => { setFormDoc({ ...post, 'first_name': e.target.value}) }} />
+                <input type="text" id='first_name' className="form-control rounded" placeholder="Enter your first name" required="required" value={post?.first_name} onChange={(e) => { setFormDoc({ ...post, 'first_name': e.target.value}) }} />
                 {errorFirstname && (
                   <p className="text-danger"> {errorFirstname} </p>
                 )}
                 </div>
                 <div className="col-md-6">
                 <label htmlFor="lastname" style={{fontWeight:'bold'}}>Last Name</label>
-                <input type="text" id='last_name' className="form-control" placeholder="Enter your last name" required="required" value={post?.last_name} onChange={(e) => { setFormDoc({ ...post, 'last_name': e.target.value}) }} />
+                <input type="text" id='last_name' className="form-control rounded" placeholder="Enter your last name" required="required" value={post?.last_name} onChange={(e) => { setFormDoc({ ...post, 'last_name': e.target.value}) }} />
                 {errorLastname && (
                   <p className="text-danger"> {errorLastname} </p>
                 )}
                 </div>
                 <div className="col-md-12">
                 <label htmlFor="email" style={{fontWeight:'bold'}}>Email</label>
-                <input type="email" id='email' className="form-control" placeholder="yourname@email.com" required="required" value={post?.email} onChange={(e) => { setFormDoc({ ...post, 'email': e.target.value}) }}/>
+                <input type="email" id='email' className="form-control rounded" placeholder="yourname@email.com" required="required" value={post?.email} onChange={(e) => { setFormDoc({ ...post, 'email': e.target.value}) }}/>
                 {errorEmail && (
                   <p className="text-danger"> {errorEmail} </p>
                 )}
 
                 <label htmlFor="message" id='message' style={{fontWeight:'bold'}}>Message</label>
-                <textarea  className="form-control" rows="5" placeholder="send me a message and i'll reply you as soon as possible" required="required" value={post?.message} onChange={(e) => { setFormDoc({ ...post, 'message': e.target.value}) }}/>
+                <textarea  className="form-control rounded" rows="5" placeholder="send me a message and i'll reply you as soon as possible" required="required" value={post?.message} onChange={(e) => { setFormDoc({ ...post, 'message': e.target.value}) }}/>
                 {errorMessage && (
                   <p className="text-danger"> {errorMessage} </p>
                 )}
@@ -93,7 +93,7 @@ const Contact = () => {
                 <input type="checkbox" className='mt-3 mx-2'/> You agree to providing your data to {name} who may contact you  
                 </div>
                 <div className="col-md-12">
-                <button id='btn__submit' onClick={handleSubmit} className="btn btn-primary mt-4" style={{height:'40px',width:'100%',textDecoration:'none',color:'white',border:'#EAECF0',justifyContent:'center'}}>Send message</button>
+                <button id='btn__submit' onClick={handleSubmit} className="btn btn-primary mt-4 rounded" style={{height:'40px',width:'100%',textDecoration:'none',color:'white',border:'#EAECF0',justifyContent:'center'}}>Send message</button>
                 </div>
               </div>
 
