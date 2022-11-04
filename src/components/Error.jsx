d51ed1d327222f5e6b58a3c5ac38a9b814f2b2fb
chiscocode <patrickchukwudifu716@gmail.com>
2022-11-04 18:00:09 +0100

    update error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,7 +5,7 @@ import { Link, useNavigate } from 'react-router-dom'
 
 
 const Error = () => {
-    const navigate =  useNavigate()
+    const navigate=useNavigate()
     useEffect(() => {
         setTimeout(() => {
           navigate('/')
