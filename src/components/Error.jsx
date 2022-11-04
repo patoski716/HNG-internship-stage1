29aa888c3302a486e661a3bbdcc126b11e0807b9
chiscocode <patrickchukwudifu716@gmail.com>
2022-11-04 17:02:22 +0100

    update 404 page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 
 const Error = () => {
-    const navigate=useNavigate()
-    useEffect(() => {
-        setTimeout(() => {
-          navigate('/')
-        }, 3000)
-      }, [])
+    // const navigate=useNavigate()
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //       navigate('/')
+    //     }, 9000)
+    //   }, [])
   return (
     <div className='container'>
         
@@ -19,11 +19,24 @@ const Error = () => {
                 <div className="col-md-12 mx-auto">
                 <p className='text-primary'>404 error</p>
                 <h1 style={{fontWeight:'bolder',fontSize:'2rem',color:'black'}}>We can't find that page</h1>
-                <p>Sorry, the page you are looking for doesn't exist</p>
+                
+                <p className='bigscreen'>Sorry, the page you are looking for doesn't exist</p>
+                <p className='smallscreen'>Sorry, the page you are looking for doesn't exist or has been moved.</p>
                 </div>
-                <div className="">
-                    <a href="/"  className="btn btn-white mx-2" id="buttom">&#8592;Go back</a>
-                    <a href="/" className="btn btn-primary" id="top">Take me home</a>
+                <div className="bigscreen">
+                    <Link to="/"  className="btn btn-white mx-2" >&#8592;Go back</Link>
+                    <Link to="/" className="btn btn-primary text-white" >Take me home</Link>
+                </div>
+
+                <div className="smallscreen">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <Link to="/" className="btn btn-primary" style={{height:'50px',width:'100%',textDecoration:'none',color:'white',justifyContent:'center'}}>Take me home</Link>    
+                        </div>
+                        <div className="col-md-6">
+                            <Link to="/"  className="btn btn-white mx-2" style={{height:'50px',width:'100%',textDecoration:'none',color:'black',justifyContent:'center'}}>&#8592; Go back</Link>
+                        </div>
+                    </div>
                 </div>
                 
 
