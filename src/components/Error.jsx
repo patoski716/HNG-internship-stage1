bcaa421f5036ff71ebae72fdb4151def8db21bba
chiscocode <patrickchukwudifu716@gmail.com>
2022-11-04 15:01:12 +0100

    update 404 page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
 
 const Error = () => {
+    const navigate=useNavigate()
+    useEffect(() => {
+        setTimeout(() => {
+          navigate('/')
+        }, 3000)
+      }, [])
   return (
     <div className='container'>
         
